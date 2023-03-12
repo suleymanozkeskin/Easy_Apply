4e9abb69c5e9fbcc05a8f27e9919d1127080223d
suleymanozkeskin <75538999+suleymanozkeskin@users.noreply.github.com>
2023-03-12 17:50:14 +0100

    update server.ts

--- server.ts
+++ server.ts
@@ -4,7 +4,7 @@ import { rapidApiKey, rapidApiHost } from './config';
 const options = {
   method: 'GET',
   url: 'https://jsearch.p.rapidapi.com/search',
-  params: {query: 'Python developer in Texas, USA', num_pages: '1'},
+  params: {query: 'Python developer in Germany', num_pages: '1'},
   headers: {
     'X-RapidAPI-Key': rapidApiKey,
     'X-RapidAPI-Host': rapidApiHost,
@@ -17,3 +17,4 @@ axios.request(options).then(function (response) {
   console.error(error);
 });
 
+// to run this file, use the command: ts-node server.ts
